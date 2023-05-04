f0080d597cf8c796804dd4850fb5a8d0c32d8b5d
nguyenhoangthienchi <jayesd3v@gmail.com>
2023-05-05 00:46:49 +0700

    implement image uploading

--- backend/src/lambda/http/generateUploadUrl.ts
+++ backend/src/lambda/http/generateUploadUrl.ts
@@ -3,24 +3,39 @@ import 'source-map-support/register'
 import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda'
 import * as middy from 'middy'
 import { cors, httpErrorHandler } from 'middy/middlewares'
-
-import { createAttachmentPresignedUrl } from '../../businessLogic/todos'
-import { getUserId } from '../utils'
+import { createAttachmentPresignedUrl } from '../../logic/todos'
+import { createLogger } from '../../utils/logger'
+const logger = createLogger('uploadTodoHandler')
 
 export const handler = middy(
   async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
-    const todoId = event.pathParameters.todoId
-    // TODO: Return a presigned URL to upload a file for a TODO item with the provided id
-    
+    try {
+      const presignedUrl = await createAttachmentPresignedUrl(event)
 
-    return undefined
+      return {
+        statusCode: 200,
+        headers: {
+          'Access-Control-Allow-Origin': '*'
+        },
+        body: JSON.stringify({
+          uploadUrl: presignedUrl
+        })
+      }
+    } catch (error) {
+      logger.error(`Error creating upload image: ${error.message}`)
+
+      return {
+        statusCode: 500,
+        body: JSON.stringify({
+          error: 'Could not upload attachment !'
+        })
+      }
+    }
   }
 )
 
-handler
-  .use(httpErrorHandler())
-  .use(
-    cors({
-      credentials: true
-    })
-  )
+handler.use(httpErrorHandler()).use(
+  cors({
+    credentials: true
+  })
+)
